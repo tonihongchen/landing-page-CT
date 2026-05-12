04cd2fade9c2a2a68bb84f48feec91c9fa4031b4
chentoni <tonihongchen@gmail.com>
2026-05-12 13:52:51 +0200

    Enhance contact form layout and styling - Updated form structure with input fields for name, email, and message - Improved styling for form section and elements

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ fetch('header.html')
 .then(datos => {
     document.getElementById('header').innerHTML = datos;
 });
+
+fetch('footer.html')
+.then(respuesta => respuesta.text())
+
+.then(datos => {
+    document.getElementById('footer').innerHTML = datos;
+});
